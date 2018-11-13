3b712b23b254a5a042e7b735cf88a0fd61839f1e
Sebastian Papararo <s.papararo@uea.ac.uk>
2018-11-13 13:17:00 +0000

    Started work on making a basic webpage to display the chatbot

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("test")
+from flask import Flask, g, render_template
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+
+    return render_template('index.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', debug=True)
