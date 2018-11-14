33beb4e2f1a91a48cc22d1425bdb9832516c3f93
Nick <N.Lilly@uea.ac.uk>
2018-11-14 18:49:22 +0000

    Did some more research into this and found some useful commands, especially at the bottom, will help us define words correctly.

--- nlpu.py
+++ nlpu.py
@@ -4,6 +4,7 @@ from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import wordnet
 ##############Book examle and practice#############
 #nltk.download()
 # from nltk.book import *
@@ -47,5 +48,63 @@ for q in filtered_sent:
     lemmated.append(lem.lemmatize(q,"v"))
 print("Lemmated:\t\t\t",lemmated)
 
+#########Prints all the words and the tags that each word comes under
 print(nltk.pos_tag(lemmated))
+
+#https://www.geeksforgeeks.org/get-synonymsantonyms-nltk-wordnet-python/
+
+
+# Then, we're going to use the term "program" to find synsets like so:
+syns = wordnet.synsets("book")# use .v to make it a verb (which is what we want)
+
+# An example of a synset:
+print(syns[0].name())
+
+# Just the word:
+print(syns[0].lemmas()[0].name())
+
+# Definition of that first synset:
+print(syns[0].definition())
+
+# Examples of the word in use in sentences:
+print(syns[0].examples())
+
+
+synonyms = []
+antonyms = []
+
+for syn in wordnet.synsets('book.v.01'):
+    for l in syn.lemmas():
+        synonyms.append(l.name())
+        if l.antonyms():
+            antonyms.append(l.antonyms()[0].name())
+
+print(set(synonyms))
+print(set(antonyms))
+
+
+# Let's compare the noun of "ship" and "boat:"
+
+w1 = wordnet.synset('book.v.01') # v here denotes the tag verb
+w2 = wordnet.synset('reserve.v.01')
+print(w1.wup_similarity(w2))
+
+#This is very useful as it will help us define words, i.e. book means to book and not an actual book
+wordnet.synset('book.v.01').frame_ids()
+
+for lemma in wordnet.synset('book.v.01').lemmas():
+    print(lemma, lemma.frame_ids())
+    print(" | ".join(lemma.frame_strings()))
+
+
+
+
+#synsets
+
+# class WordReplacer(object):
+#   def __init__(self, word_map):
+#     self.word_map = word_map
+#
+#   def replace(self, word):
+#     return self.word_map.get(word, word)
 
